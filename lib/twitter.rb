b2b662e30fe798ba1526fcc47d72044f6a7f1d8a
Luca Spiller <luca@stackednotion.com>
2009-06-09 22:48:08 +0100

    Update twitter class to load config

---
+++ lib/twitter.rb
@@ -0,0 +1,11 @@
+gem 'twitter'; require 'twitter'
+
+begin
+	@@config = YAML::load(open('lib/config'))
+rescue
+	# write sample
+	File.open('lib/config', 'w') { |f| f.write({ 'token' => 'edit', 'secret' => 'edit' }.to_yaml) }
+	
+	puts "Please edit 'lib/config'"
+	exit
+end
