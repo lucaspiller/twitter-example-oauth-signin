4fa933912fcd28fc60325fa8237fc0f8384d8868
Luca Spiller <luca@stackednotion.com>
2009-06-10 22:37:33 +0100

    Make it work

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -4,7 +4,7 @@ begin
 	@@config = YAML::load(open('lib/config'))
 rescue
 	# write sample
-	File.open('lib/config', 'w') { |f| f.write({ 'token' => 'edit', 'secret' => 'edit' }.to_yaml) }
+	File.open('lib/config', 'w') { |f| f.write({ 'key' => 'edit', 'secret' => 'edit' }.to_yaml) }
 	
 	puts "Please edit 'lib/config'"
 	exit
--- runner.rb
+++ runner.rb
@@ -2,12 +2,52 @@ require 'rubygems'
 $:.unshift File.dirname(__FILE__) + '/sinatra/lib'
 require 'sinatra'
 
-require 'lib/boot'
+configure do
+	set :sessions, true
+	require 'lib/boot'
+end
 
 before do
-	@user = session[:user]
+	@auth = Twitter::OAuth.new @@config['key'], @@config['secret'], :sign_in => true
+	
+	unless session[:access_token].nil? or session[:access_secret].nil?
+		@auth.authorize_from_access session[:access_token], session[:access_secret]
+	end
+	
+	@twitter = Twitter::Base.new(@auth)
 end
 
 get '/' do
-	haml :index
+	@tweets = @twitter.user_timeline
+	haml :status
+end
+
+get '/unauthorised' do
+	haml :unauthorised
+end
+
+# auth
+get '/signin' do
+	session[:request_token] = @auth.request_token.token
+	session[:request_token_secret] = @auth.request_token.secret
+	
+	redirect @auth.request_token.authorize_url.gsub('authorize', 'authenticate')
+end
+
+get '/auth' do
+	@auth.authorize_from_request session[:request_token], session[:request_token_secret]
+		
+	session[:access_token] = @auth.access_token.token
+	session[:access_secret] = @auth.access_token.secret
+		
+	redirect '/'
+end
+
+get '/signout' do
+	session.clear
+	redirect '/'
+end
+
+error Twitter::Unauthorized do
+	redirect '/unauthorised'
 end
